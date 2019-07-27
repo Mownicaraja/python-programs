b4336e5811c8c108211421fae053d1eb47e15353
Mownicaraja <37542800+Mownicaraja@users.noreply.github.com>
2019-07-27 19:25:34 +0530

    Update fact-n.py

--- fact-n.py
+++ fact-n.py
@@ -1,8 +1,8 @@
-no=int(input())
+N=int(input())
 f=1
-if(no<=20 and no==0):
+if(N<=20 and N==0):
     print("1")
-elif(no<20):
-    for i in range(1,no+1):
+elif(N<=20):
+    for i in range(1,N+1):
         f=f*i
     print(f)
